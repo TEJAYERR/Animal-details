cc3a992705b2224d3b55a5856a6cd4ad25851187
TEJAYERR <146433384+TEJAYERR@users.noreply.github.com>
2024-10-16 20:14:46 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="navbar.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+            <section class="left-nav">
+                <h2>Animals</h2>
+            </section>
+            <section class="right-nav">
+                <ul>
+                    <li><a href="../html/navbar.html">Animals</a></li>
+                    <li><a href="../html/birds.html">Birds</a></li>
+                    <li><a href="../html/microorganisms.html">Micro-orgaisms</a></li>
+                    <a href="" id="button">Login</a>
+                </ul>
+            </section>
+        </nav>
+    </header>
+    <div class="text">
+        <h1>Welcome to the amazing wesite</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis numquam, ipsa, quis voluptatum sapiente, molestiae debitis exercitationem fuga praesentium doloribus incidunt dolorum impedit repellat perferendis maxime esse explicabo unde ad!</p>
+    </div>
+    <div class="container">
+        <div class="card card1">
+            <h3 id="name">Aardvark</h3>
+            <section class="sect">
+                <p class="p">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore magni odio maxime voluptatum dolorum nesciunt repellendus omnis id sint optio, nisi adipisci perspiciatis porro ut nulla error dicta voluptatibus. Quae? its the actual matter about the thing inaaaaaaaaaaaaaaaaaaaaaaaaaaa     aaaaaaaaaaaaaa above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Aardwolf</h3>
+            <section class="sect">
+                <p class="p">adipisicing elit. Repudiandae natus, cum deleniti voluptatibus corrupti blanditiis provident non veniam, vero recusandae distinctio laudantium labore, similique ratione!its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Addax</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Adelie Penguin</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">African Buffalo</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">African Elephant</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">African Forest Elephant</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">African Lions</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">African Penguin</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Name</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Name</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Name</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Name</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Name</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+        <div class="card card1">
+            <h3 id="name">Name</h3>
+            <section class="sect">
+                <p class="p">its the actual matter about the thing in above picture</p>
+            </section>
+        </div>
+    </div>
+</body>
+</html>
